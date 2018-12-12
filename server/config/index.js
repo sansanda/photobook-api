6385a0eb68479bbf66e9cf0b194c822931e14ed6
sansanda <dsanchezsanc@uoc.edu>
2018-12-12 18:04:32 +0100

    working on handle-erros-and-log

--- server/config/index.js
+++ server/config/index.js
@@ -2,7 +2,7 @@ require('dotenv').config('');
 
 const config = {
   server: {
-    hostname: process.env.SERVER_HOSTNAME,
+    hostname: process.env.SERVER_IP,
     port: process.env.SERVER_PORT,
   },
 };
