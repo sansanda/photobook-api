0fe9786c75ddbd54ada6d5ea9b37956e59fb3f88
sansanda <dsanchezsanc@uoc.edu>
2018-12-13 10:23:35 +0100

    Add middleware Morgan and winston and modified /server/index.js

--- server/index.js
+++ server/index.js
@@ -1,31 +1,35 @@
 const express = require('express');
+const morgan = require('morgan');
+const logger = require('winston');
 
 const app = express();
 
+// Setup middleware
+app.use(morgan('common'));
+
+// Routes
 app.get('/', (req, res, next) => {
+  logger.info('API root');
   res.json({
-    message: 'Welcome to the API'
+    message: 'Welcome to the API',
   });
 });
 
-app.use( (req, res, next) => {
+// Handle middleware errors
+app.use((req, res, next) => {
+  logger.info('Route not found');
   res.status(404);
   res.json({
-    error: 'Error. Route not found'
+    error: 'Route not found',
   });
 });
 
-app.use( (err, req, res, next) => {
-  const {
-    statusCode = 500,
-    message,
-  } = err;
-
-  res.status(statusCode);
+app.use((err, req, res, next) => {
+  logger.error(`Error: ${err}`);
+  res.status(500);
   res.json({
-    message,
+    error: `${err}`,
   });
 });
-
 
 module.exports = app;
