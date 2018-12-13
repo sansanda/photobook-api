def1c7050c1a32aa5b14bce80c2e05d65b75edcd
sansanda <dsanchezsanc@uoc.edu>
2018-12-13 12:51:37 +0100

    Handling params from request

--- server/api/v1/posts/controller.js
+++ server/api/v1/posts/controller.js
@@ -1,22 +1,35 @@
-exports.all = (req, res, next) => {
-    res.json({});
-};
-
-
 //CRUD 
 
+//ALL POSTS
+
+//GET ALL
+exports.all = (req, res, next) => {
+    res.json(
+        [{ "_id": 1, "title": "Vacation" },
+        { "_id": 2, "title": "Jogging" }]
+    );
+};
+
+//CREATE POSTS
 exports.create = (req, res, next) => {
     res.json({});
 };
 
+
+
+//ONE POST
+
+//READ ONE POST
 exports.read = (req, res, next) => {
-    res.json({});
+    res.json({ "_id": req.params.id });
 };
 
+//UPDATE ONE POST
 exports.update = (req, res, next) => {
     res.json({});
 };
 
+//DELETE ONE POST
 exports.delete = (req, res, next) => {
     res.json({});
 };
